04c5b732f2cccfb43beb009e8f05e18e91ed239e
CK-NASU\I.T Dept <jonasreycian@gmail.com>
2024-04-26 22:26:19 +0800

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcServer;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("https://localhost:7068");
+
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+       new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.ReadLine();
---
+++ GrpcServer/Services/IGreeterService.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public interface IGreeterService
+    {
+        Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context);
+    }
+}
